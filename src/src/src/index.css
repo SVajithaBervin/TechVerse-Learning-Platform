54560d30452d763b441e0d29c6b9be5fb4fdffc2
SVajithaBervin <mashaallah.janofer786.24.687@gmail.com>
2026-08-31 10:43:53 +0530

    Add global styles
    Added global styles for the TechVerse Learning Platform.

--- src/src/src/index.css
+++ src/src/src/index.css
@@ -1,3 +1,8 @@
+/* =========================================================
+   TechVerse Learning Platform
+   Global Styles
+   ========================================================= */
+
 * {
   margin: 0;
   padding: 0;
@@ -10,5 +15,34 @@ html {
 
 body {
   margin: 0;
+  padding: 0;
   font-family: Arial, Helvetica, sans-serif;
+  min-width: 320px;
+}
+
+button,
+input,
+textarea,
+select {
+  font-family: inherit;
+}
+
+button {
+  cursor: pointer;
+}
+
+img {
+  max-width: 100%;
+  display: block;
+}
+
+a {
+  text-decoration: none;
+}
+
+ul,
+ol {
+  margin: 0;
+  padding: 0;
+  list-style: none;
 }
