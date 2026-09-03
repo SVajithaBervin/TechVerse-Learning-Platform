65070fbc8b5770bd363abacc92ec960d7c31b9ac
SVajithaBervin <mashaallah.janofer786.24.687@gmail.com>
2026-09-03 18:29:11 +0530

    Update Welcome.jsx

--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -1,1 +1,11 @@
+import { useEffect } from "react";
 
+function Welcome() {
+  useEffect(() => {
+    document.title = "TechVerse - Learning & Online Assessment Platform";
+  }, []);
+
+  return null;
+}
+
+export default Welcome;
